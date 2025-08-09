8404dd75977c42f320188ca9f3f83bc908c7bf34
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-08-09 16:37:32 +0500

    Added full project files

---
+++ contect-us/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,52 @@
+import styles from './ConatctForm.module.css'
+import Button from '../Button/Button'
+import {MdMessage} from "react-icons/md";
+import {FaPhoneAlt} from "react-icons/fa";
+import {HiMail} from "react-icons/hi";
+const ContactForm = () => {
+  return (
+   <section className={styles.container}>
+    <div className={styles.contact_form}>
+    
+    <div className={styles.top_btn}><Button text="VIA SUPPORT CHA"
+    icon={<MdMessage fontSize="24px" />}
+    />
+    <Button text="VIA CALL"
+    icon={<FaPhoneAlt fontSize="24px" />}
+    />
+  </div>
+    <Button isOutline={true} text="VIA EMAIL FORM"
+    icon={<HiMail fontSize="24px" />}
+    />
+
+    <form  action="">
+       <div className={styles.form_control}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name='name'/>
+        </div> 
+       <div className={styles.form_control}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name='email'/>
+        </div> 
+       <div className={styles.form_control}>
+        <label htmlFor="text">Text</label>
+        <textarea  name='text' rows="8" />
+        
+        </div> 
+        <div style={{
+            display: 'flex',
+            justifyContent:"end",
+        }}><Button  text="SUBMIT" /></div>
+
+    </form>
+</div>
+   
+    
+    <div className={styles.contact_image}>
+        <img src="/images/contact.svg" alt="contact image" />
+    </div>
+   </section>
+  )
+}
+
+export default ContactForm
